63aaa8f44d9d284d8e81b7c8b77b9ace19c8070e
MESHACH ISHAYA <meshach@ashinity.com>
2026-08-19 11:43:12 +0100

    Update Ashinity partner brands

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,18 +1,20 @@
 import Image from "next/image";
 
 import aktivImg from "@/public/assets/images/aktiv.png";
-import aurallyImg from "@/public/assets/images/aurallyLogo.png";
 import farmerseaImg from "@/public/assets/images/farmersea-logo.svg";
+import omnipoolImg from "@/public/assets/images/omnipool-logo.svg";
 import oscirisImg from "@/public/assets/images/osciris-logo.svg";
 import quickcareImg from "@/public/assets/images/quickcare.png";
 import spendaImg from "@/public/assets/images/spenda.png";
 import spinlotoImg from "@/public/assets/images/SpinlottoLogo.png";
+import veriagentPayImg from "@/public/assets/images/veriagent-pay-logo.svg";
 
 const ventures = [
   { image: farmerseaImg, name: "Farmersea" },
   { image: oscirisImg, name: "OSCIRIS" },
+  { image: veriagentPayImg, name: "VeriAgent Pay" },
+  { image: omnipoolImg, name: "Omnipool" },
   { image: spinlotoImg, name: "Spinlotto" },
-  { image: aurallyImg, name: "Aurally" },
   { image: spendaImg, name: "Spenda" },
   { image: aktivImg, name: "Aktiv" },
   { image: quickcareImg, name: "Quickcare" },
